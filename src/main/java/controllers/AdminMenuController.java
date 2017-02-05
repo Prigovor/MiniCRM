88a591310dbe99106fbdc62f507d9cecc1046d02
drfauct <qwerty12345>
2017-02-05 16:40:11 +0200

    ControllerMethods

--- src/main/java/controllers/AdminMenuController.java
+++ src/main/java/controllers/AdminMenuController.java
@@ -4,4 +4,26 @@ package controllers;
  * Created by Жека on 2/5/2017.
  */
 public class AdminMenuController {
+
+    public void addNewEmployee(){
+
+    }
+
+    public void changeEmployee(){
+
+    }
+
+    public void addNewUser(){
+
+    }
+
+    public void removeUser(){
+
+    }
+
+    public void backToPrevMenu(){
+
+    }
+
+
 }
--- src/main/java/controllers/AuthorizationMenuController.java
+++ src/main/java/controllers/AuthorizationMenuController.java
@@ -4,4 +4,8 @@ package controllers;
  * Created by Жека on 2/5/2017.
  */
 public class AuthorizationMenuController {
+
+    public void entry (){
+
+    }
 }
