d97450a24950b7ccae12ae084e0c24c1cc06d3fc
Prigovor <prigovors@gmail.com>
2017-02-07 21:55:22 +0200

    viewCommit

---
+++ src/main/java/controllers/ControlledScreen.java
@@ -0,0 +1,11 @@
+package controllers;
+
+/**
+ * Created by Prigovor on 05.02.2017.
+ */
+@FunctionalInterface
+public interface ControlledScreen {
+
+    public void setScreenParent(ScreensController screenPage);
+
+}
